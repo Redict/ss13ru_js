9e70a35b43d3be2216281c48cd9137d2acf9b26e
bo20202 <bo20202@users.noreply.github.com>
2018-07-03 23:01:22 +0300

    Add webpack bundling (#2)
    * Add .gitignore
    
    * Add imports to provide webpack support
    
    * Remove app.js script, add data.js script
    
    * Add package.json
    
    * Add webpack config
    
    * Update README.md
    
    * Update README.md
    
    * Add ready-to-use output
    
    * Fix canvas resize
    
    * Fix styles
    
    * Update bundle

---
+++ data.js
@@ -0,0 +1,74 @@
+// Icons catalog on https://fontawesome.com/icons?d=gallery&m=free
+
+window.data = {
+    objects: ['assets/objects/clown.png', 'assets/objects/Jobass.png', 'assets/objects/FireExtinguisher.png'],
+    news: [{
+        name: '<img height="32" style="margin-right:7px" src="https://cdn.discordapp.com/icons/428876420979097600/a22e728c1d6c8cdc5bc312d45977ffd3.jpg"> SS13.Ru',
+        date: 1529741933990,
+        content: `Дизайн главной был обновлен.
+
+Теперь главная отвечает текущим требованиям проекта, она центрируется на новостях которые перекидываются сюда из серверов дискорда, а так же имеет плашки серверов на которых можно размещать кнопки. На старых это не было бы возможно, по причине что они сделаны на иконках, и вносить туда дополнительную информацию или модульность это сущий ад.
+
+Дизайн финальным не является и будет дорабатываться дальше. В частности, наверху вы можете видеть банеры из рандомных картинок из станции, которые мы будем наполнять чем-то более оригинальным и внятным. Это так же касается других деталей, которые мы будем трогать напильником.`,
+    }],
+    servers: [{
+        name: 'The Neekothing',
+        icon: 'https://ss13.ru/img/neekothing.png',
+        online: true,
+        players: 1337,
+        description: 'Психоделическая тоталитарная утопия. В силу интернациональности, общение ведётся на смеси из ломанного английского и русского мата.',
+        address: 'byond://50.35.188.8:50455',
+        links: [
+            {what: 'discord', text: "Discord", href: 'https://discord.gg/8CD8gVf'}
+        ],
+    },{
+        name: 'Yellow Circus',
+        icon: 'https://ss13.ru/img/Server_3.svg',
+        online: true,
+        players: 16,
+        description: 'Почти всегда самый свежий форк ТГ со своими фичами и шутеечками. Крайне ненавязчивый отыгрыш, динамичный геймплей, Вьетнам, растяжки, Омск.',
+        address: 'byond://ss13.ru:2509',
+        links: [
+            {what: 'discord', text: "Discord", href: 'https://discordapp.com/invite/K57CEvZ'},
+            {what: 'github', text: "Github", href: 'https://github.com/Rampoch/Chaotic-Onyx'},
+            {what: 'wikipedia-w', text: "Wiki", href: 'https://wiki.ss13.ru/index.php?title=Chaotic_Onyx'},
+        ],
+    },{
+        name: 'Navy Torch',
+        icon: 'https://ss13.ru/img/Server_4.svg',
+        online: true,
+        players: 19,
+        description: 'Navy Torch — это ненавязчивый отыгрыш в условиях постоянно нарастающего напряжения, происходящий на небольшом исследовательском судне Факел, откуда на регулярной основе отправляются экспедиции на неизвестные объекты в безграничном космосе.',
+        address: 'byond://ss13.ru:2507',
+        links: [
+            {what: 'discord', text: "Discord", href: 'https://discordapp.com/invite/K57CEvZ'},
+            {what: 'github', text: "Github", href: 'https://github.com/Rampoch/Chaotic-Onyx'},
+            {what: 'wikipedia-w', text: "Wiki", href: 'https://wiki.ss13.ru/index.php?title=Chaotic_Onyx'},
+        ],
+    }],
+    help: {
+        name: 'Помощь серверу',
+        content: `Мы приветствуем вас, дорогой посетитель. Разработка Космической Станции 13 не прекращается ни на неделю и с каждым днем умелые кодеры вводят для вас все новые и новые возможности, но, к сожалению, нам остро не хватает рабочих рук. Вам не безразлична судьба Анимуса? Желаете помочь серверу? Ознакомьтесь с этим разделом и вариантами посильной помощи, именно усилиями добровольцев наше сообщество появилось, растёт и развивается:
+
+# Кодеры
+На космической станции 13 у нас постоянная нехватка кодеров. Если вы любитель покопаться в коде, то знайте - для космической станции 13 вы бесценны. Ознакомиться со структурой нашей сборки вы можете скачав её с GitHub репозитория. Также вам может помочь наша статья All about the code, и если вы знаете английский язык, вам могут пригодится такие статьи: Coding Standards, Text Formatting и конечно же SS13 for Experienced Programmers.
+
+# Спрайтеры
+У вас есть талант к рисованию? Можете сделать так, чтобы изображение размером 32x32 у вас было похоже хоть на что-то? Тогда смело читайте эти статьи, ведь они обязаны к прочтению каждому начинающему спрайтеру: статья первая, вторая и третья.
+
+Прочитали вышеупомянутое? Тогда ознакомьтесь с небольшой статьей на вики, в которой затрагиваются самые частые ошибки начинающих спрайтеров - вы ведь не хотите учиться на своих оплошностях, не так ли? Вот вам руководство по спрайтингу на русском и английском языке. Ознакомившись с ними смело идите в раздел спрайтов на форуме, загляните в тему о заказах изображений и творите, либо нарисуйте несколько вещей в формате СС13 для души, после чего создайте уже свою собственную тему, где и выкладывайте всё нарисованное вами для использования кодерами и эдакого портфолио.
+
+# Человеки доброй воли
+Завалялась небольшая сумма на Интернет-кошельке? Не безразлична судьба сервера?
+Автоматически, через кучу разных платежных систем:
+* http://zlobilling.org/services/ss13-5
+
+Только благодаря таким людям как вы, Анимус живет и развивается. Кроме того не забывайте о разветвлённой системе привилегий, внедренной нами для донатеров, Благодаря которой вы сможете разнообразить свою игру небольшими преимуществами и красивыми наборами.
+
+На этом все - вы не можете сделать ничего из вышеперечисленного? Не печальтесь, вы помогаете серверу просто играя на нем, а также приглашая друзей. Будьте приятным собеседником и игроком - учавствуйте в обсуждениях на форуме, конференции, дополняйте статьи в открытой для редактирования Вики. Удачи, enjoy your stay!
+
+# Добрые люди:
+<table><tbody><tr><th>#</th><th>Игрок</th><th>Сумма</th></tr><tr><td align="center">221</td><td>mikag22228</td><td>86.00 RUB</td></tr><tr><td align="center">222</td><td>nickerps</td><td>80.00 RUB</td></tr><tr><td align="center">223</td><td>dobrak</td><td>75.00 RUB</td></tr><tr><td align="center">224</td><td>fixresh</td><td>70.00 RUB</td></tr><tr><td align="center">225</td><td>ricorodrigez</td><td>70.00 RUB</td></tr><tr><td align="center">226</td><td>neketoser</td><td>69.00 RUB</td></tr><tr><td align="center">227</td><td>silentenemy</td><td>66.00 RUB</td></tr><tr><td align="center">228</td><td>j3t1699</td><td>66.00 RUB</td></tr><tr><td align="center">229</td><td>salarina</td><td>64.00 RUB</td></tr><tr><td align="center">230</td><td>real66</td><td>60.00 RUB</td></tr><tr><td align="center">231</td><td>muscari</td><td>60.00 RUB</td></tr><tr><td align="center">232</td><td>chaostheorist17</td><td>60.00 RUB</td></tr><tr><td align="center">233</td><td>uberhohol</td><td>60.00 RUB</td></tr><tr><td align="center">234</td><td>ferren</td><td>58.00 RUB</td></tr><tr><td align="center">235</td><td>zippowelder</td><td>55.00 RUB</td></tr><tr><td align="center">236</td><td>nikitos32ru</td><td>55.00 RUB</td></tr><tr><td align="center">237</td><td>rully</td><td>52.00 RUB</td></tr><tr><td align="center">238</td><td>kriper666</td><td>50.00 RUB</td></tr><tr><td align="center">239</td><td>evgenm</td><td>50.00 RUB</td></tr><tr><td align="center">240</td><td>arduamino</td><td>50.00 RUB</td></tr><tr><td align="center">241</td><td>voidvoid</td><td>50.00 RUB</td></tr><tr><td align="center">242</td><td>kreeperhlc</td><td>50.00 RUB</td></tr><tr><td align="center">243</td><td>kupujiji07</td><td>50.00 RUB</td></tr><tr><td align="center">244</td><td>bigrage</td><td>50.00 RUB</td></tr><tr><td align="center">245</td><td>ipxkz</td><td>50.00 RUB</td></tr><tr><td align="center">246</td><td>hamyrappy</td><td>50.00 RUB</td></tr><tr><td align="center">247</td><td>seel112</td><td>50.00 RUB</td></tr><tr><td align="center">248</td><td>ekaterina</td><td>50.00 RUB</td></tr><tr><td align="center">249</td><td>irlizi</td><td>50.00 RUB</td></tr><tr><td align="center">250</td><td>marsianen30</td><td>45.00 RUB</td></tr><tr><td align="center">251</td><td>cookcook</td><td>44.00 RUB</td></tr><tr><td align="center">252</td><td>coffeemolka</td><td>42.00 RUB</td></tr><tr><td align="center">253</td><td>dancaf</td><td>40.00 RUB</td></tr><tr><td align="center">254</td><td>gss</td><td>40.00 RUB</td></tr><tr><td align="center">255</td><td>xbillx</td><td>35.00 RUB</td></tr><tr><td align="center">256</td><td>randelf</td><td>30.00 RUB</td></tr><tr><td align="center">257</td><td>veritus</td><td>30.00 RUB</td></tr><tr><td align="center">258</td><td>valentinzastrozhnyy</td><td>30.00 RUB</td></tr><tr><td align="center">259</td><td>fighterx250</td><td>30.00 RUB</td></tr><tr><td align="center">260</td><td>regnar</td><td>30.00 RUB</td></tr><tr><td align="center">261</td><td>theatalis</td><td>29.00 RUB</td></tr><tr><td align="center">262</td><td>alexdc546</td><td>28.00 RUB</td></tr><tr><td align="center">263</td><td>timmiel</td><td>25.00 RUB</td></tr><tr><td align="center">264</td><td>dvacheroldfag</td><td>24.00 RUB</td></tr><tr><td align="center">265</td><td>auesti</td><td>23.00 RUB</td></tr><tr><td align="center">266</td><td>kostenoj</td><td>21.00 RUB</td></tr><tr><td align="center">267</td><td>ainferno</td><td>20.00 RUB</td></tr><tr><td align="center">268</td><td>paukan</td><td>20.00 RUB</td></tr><tr><td align="center">269</td><td>sayros2014</td><td>20.00 RUB</td></tr><tr><td align="center">270</td><td>zhmeln</td><td>20.00 RUB</td></tr><tr><td align="center">271</td><td>geforce44ru</td><td>20.00 RUB</td></tr><tr><td align="center">272</td><td>moonmen</td><td>20.00 RUB</td></tr><tr><td align="center">273</td><td>rickmangay</td><td>17.00 RUB</td></tr><tr><td align="center">274</td><td>shenya8900</td><td>15.00 RUB</td></tr><tr><td align="center">275</td><td>toneftw</td><td>15.00 RUB</td></tr><tr><td align="center">276</td><td>fixer32</td><td>15.00 RUB</td></tr><tr><td align="center">277</td><td>grib123</td><td>15.00 RUB</td></tr><tr><td align="center">278</td><td>groindestroyer</td><td>15.00 RUB</td></tr><tr><td align="center">279</td><td>eewww1</td><td>13.00 RUB</td></tr><tr><td align="center">280</td><td>silverpavel</td><td>13.00 RUB</td></tr><tr><td align="center">281</td><td>spacetea</td><td>11.00 RUB</td></tr><tr><td align="center">282</td><td>tabletka12</td><td>11.00 RUB</td></tr><tr><td align="center">283</td><td>melyamax</td><td>10.00 RUB</td></tr><tr><td align="center">284</td><td>spawn3222</td><td>10.00 RUB</td></tr><tr><td align="center">285</td><td>hkraken</td><td>10.00 RUB</td></tr><tr><td align="center">286</td><td>virtuoznew</td><td>10.00 RUB</td></tr><tr><td align="center">287</td><td>GruziLLo</td><td>10.00 RUB</td></tr><tr><td align="center">288</td><td>perkuze</td><td>10.00 RUB</td></tr><tr><td align="center">289</td><td>zazaby</td><td>10.00 RUB</td></tr><tr><td align="center">290</td><td>ssting</td><td>10.00 RUB</td></tr><tr><td align="center">291</td><td>makron0660</td><td>10.00 RUB</td></tr><tr><td align="center">292</td><td>koreandoggy</td><td>10.00 RUB</td></tr><tr><td align="center">293</td><td>macleod962</td><td>10.00 RUB</td></tr><tr><td align="center">294</td><td>gelium</td><td>10.00 RUB</td></tr><tr><td align="center">295</td><td>oldminer</td><td>10.00 RUB</td></tr><tr><td align="center">296</td><td>moscowchief</td><td>10.00 RUB</td></tr><tr><td align="center">297</td><td>orphanage</td><td>10.00 RUB</td></tr><tr><td align="center">298</td><td>temanet2011</td><td>10.00 RUB</td></tr><tr><td align="center">299</td><td>dan2134</td><td>9.00 RUB</td></tr><tr><td align="center">300</td><td>mainboy66</td><td>9.00 RUB</td></tr><tr><td align="center">301</td><td>vonam3d</td><td>9.00 RUB</td></tr><tr><td align="center">302</td><td>98sec</td><td>9.00 RUB</td></tr><tr><td align="center">303</td><td>finnishcutie</td><td>8.00 RUB</td></tr><tr><td align="center">304</td><td>qaratheon</td><td>8.00 RUB</td></tr><tr><td align="center">305</td><td>leeroyjenkns</td><td>6.00 RUB</td></tr><tr><td align="center">306</td><td>nigilith</td><td>5.00 RUB</td></tr><tr><td align="center">307</td><td>timuraztec</td><td>5.00 RUB</td></tr><tr><td align="center">308</td><td>kupriak</td><td>5.00 RUB</td></tr><tr><td align="center">309</td><td>noobis17</td><td>5.00 RUB</td></tr><tr><td align="center">310</td><td>woofwoof322</td><td>5.00 RUB</td></tr><tr><td align="center">311</td><td>vostok17</td><td>5.00 RUB</td></tr><tr><td align="center">312</td><td>nyasterds</td><td>5.00 RUB</td></tr><tr><td align="center">313</td><td>minesheep</td><td>5.00 RUB</td></tr><tr><td align="center">314</td><td>nodos</td><td>4.00 RUB</td></tr><tr><td align="center">315</td><td>magistertlt</td><td>4.00 RUB</td></tr><tr><td align="center">316</td><td>thewoien</td><td>3.00 RUB</td></tr><tr><td align="center">317</td><td>koroza</td><td>3.00 RUB</td></tr><tr><td align="center">318</td><td>vallat</td><td>3.00 RUB</td></tr><tr><td align="center">319</td><td>bennymontana</td><td>2.00 RUB</td></tr><tr><td align="center">320</td><td>hanter0718</td><td>2.00 RUB</td></tr><tr><td align="center">321</td><td>mimelover</td><td>2.00 RUB</td></tr><tr><td align="center">322</td><td>rero</td><td>1.00 RUB</td></tr><tr><td align="center">323</td><td>mamuebal</td><td>1.00 RUB</td></tr><tr><td align="center">324</td><td>alexix1989</td><td>1.00 RUB</td></tr><tr><td align="center">325</td><td>randysandy</td><td>1.00 RUB</td></tr><tr><td align="center">326</td><td>huntersa</td><td>1.00 RUB</td></tr><tr><td align="center">327</td><td>bishehlop</td><td>1.00 RUB</td></tr><tr><td align="center">328</td><td>2feycot</td><td>1.00 RUB</td></tr><tr><td align="center">329</td><td>plastmasa</td><td>1.00 RUB</td></tr><tr><td align="center">330</td><td>tyndser</td><td>1.00 RUB</td></tr></tbody></table>
+`
+    }
+}
